9d5e6543cee5e210fccffe80846ed36cb08799ec
CATEVA26 <119013401+CATEVA26@users.noreply.github.com>
2024-07-14 18:55:57 -0500

    NavegacionPantallasV1

---
+++ 02-Deberes/Formula1D2/app/src/main/java/com/example/formula1cslv/GrandPrix.kt
@@ -0,0 +1,11 @@
+package com.example.formula1cslv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GrandPrix : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grand_prix)
+    }
+}
