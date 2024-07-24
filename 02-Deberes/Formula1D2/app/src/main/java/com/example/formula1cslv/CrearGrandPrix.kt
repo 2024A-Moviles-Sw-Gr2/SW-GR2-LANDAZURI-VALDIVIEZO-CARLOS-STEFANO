f54fea6135e0b6d48153836087390b91b91da9ca
CATEVA26 <119013401+CATEVA26@users.noreply.github.com>
2024-07-23 19:26:09 -0500

    "implementacion CRUD SQLite"

---
+++ 02-Deberes/Formula1D2/app/src/main/java/com/example/formula1cslv/CrearGrandPrix.kt
@@ -0,0 +1,11 @@
+package com.example.formula1cslv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearGrandPrix : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_grand_prix)
+    }
+}
---
+++ 02-Deberes/Formula1D2/app/src/main/java/com/example/formula1cslv/EditarAuto.kt
@@ -0,0 +1,11 @@
+package com.example.formula1cslv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarAuto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_auto)
+    }
+}
