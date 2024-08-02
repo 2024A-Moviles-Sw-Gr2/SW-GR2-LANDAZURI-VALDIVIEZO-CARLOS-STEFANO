4746e16e3a043f7e66397c2da012c527665808f6
CATEVA26 <carlos.landazuri@epn.edu.ec>
2024-08-02 15:24:14 -0500

    Front

--- 02-Deberes/Deber03/app/src/main/java/com/example/deber03/MainActivity.kt
+++ 02-Deberes/Deber03/app/src/main/java/com/example/deber03/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.example.deber03
 
+import android.graphics.Color
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         inicializarVista()
+        configurarBottomNavigationView()
+    }
+
+    private fun configurarBottomNavigationView() {
+
     }
 
     private fun inicializarVista() {
+
         val listaSecciónProductos = listOf(
             "Packs de ofertas,Cualquier 3+ artículos | Desde $0.99>" to listOf(R.drawable.carcasa,R.drawable.soporte_celular,R.drawable.holster,R.drawable.funko),
             "SuperOfertas,Solo hoy.Termina: 09:18:56>" to listOf(
